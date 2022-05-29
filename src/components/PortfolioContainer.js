d7877b40f78e4aec5d9c30425bc47ac579bae791
Elaine Deng <qd9069@gmail.com>
2022-05-29 14:01:30 -0700

    Added logic for rendering page conditionally

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('AboutMe');
+
+  // This function is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'Resume':
+        return <Resume />;
+      case 'Contact':
+        return <Contact />;
+      case 'Portfolio':
+        return <Portfolio />;
+      default:
+        return <AboutMe />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage function which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
