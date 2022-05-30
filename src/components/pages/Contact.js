80af06dbda845b99ebb658d0aae9c8bc8a89e24e
Elaine Deng <qd9069@gmail.com>
2022-05-29 22:12:07 -0700

    Added avatar and Contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,85 @@
-import React from 'react';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        example
-      </p>
-    </div>
+    // <Card style={{ maxWidth: 550, margin: "0 auto", padding: "20px 5px" }} >
+    <Card style={{ maxWidth: 650, padding: "20px 5px" }} sx={{my: 7, ml: 9}}>
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          Contact
+        </Typography>
+        <form>
+          <Grid container spacing={1}>
+            <Grid xs={12} item>
+              <Typography
+                gutterBottom
+                variant="body2"
+                component="p"
+                color="textSecondary"
+              >
+                Name:
+              </Typography>
+              <TextField
+                // label="Name"
+                placeholder="Enter your name"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Typography
+                gutterBottom
+                variant="body2"
+                component="p"
+                color="textSecondary"
+              >
+                Email address:
+              </Typography>
+              <TextField
+                // label="Email address"
+                placeholder="Enter email"
+                fullWidth
+                required
+                type="email"
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Typography
+                gutterBottom
+                variant="body2"
+                component="p"
+                color="textSecondary"
+              >
+                Message:
+              </Typography>
+              <TextField
+                // label="Message"
+                placeholder="Type your message here"
+                fullWidth
+                required
+                multiline
+                rows={10}
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Button
+                size="small"
+                variant="contained"
+                color="primary"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
