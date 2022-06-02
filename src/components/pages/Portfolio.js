6394ea043b5000fbe0caf0f588f0b783bf941a7e
Elaine Deng <qd9069@gmail.com>
2022-06-01 19:32:57 -0700

    Modified code to use map for projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,15 +4,61 @@ import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
-import doggo from '../images/doggo-meet-up.png';
-import weather from '../images/weather-dashboard.png';
-import codeQuiz from '../images/code-quiz.png';
-import unblockUnwind from '../images/unblock-unwind.png';
-import noteTaker from '../images/note_taker.png';
-import jate from '../images/jate.png';
+import doggo from "../images/doggo-meet-up.png";
+import weather from "../images/weather-dashboard.png";
+import codeQuiz from "../images/code-quiz.png";
+import unblockUnwind from "../images/unblock-unwind.png";
+import noteTaker from "../images/note_taker.png";
+import jate from "../images/jate.png";
 // import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function Portfolio() {
+  const projects = [
+    {
+      pImg: doggo,
+      pName: "Doggo Meet Up",
+      pSkills: "JavaScript/Handlebars/CSS",
+      pRepo: "https://github.com/qd9069/doggo-meet-up",
+      pDeployed: "https://hidden-reaches-52880.herokuapp.com/",
+    },
+    {
+      pImg: weather,
+      pName: "Weather Dashboard",
+      pSkills: "HTML/CSS/JavaScript/Bootstrap",
+      pRepo: "https://github.com/qd9069/weather_dashboard_server_apis_project",
+      pDeployed:
+        "https://qd9069.github.io/weather_dashboard_server_apis_project/",
+    },
+    {
+      pImg: codeQuiz,
+      pName: "Code Quiz",
+      pSkills: "HTML/CSS/JavaScript",
+      pRepo: "https://github.com/qd9069/code_quiz_api_project",
+      pDeployed: "https://qd9069.github.io/code_quiz_api_project/",
+    },
+    {
+      pImg: unblockUnwind,
+      pName: "Unblock-Unwind",
+      pSkills: "HTML/CSS/JavaScript/jQuery",
+      pRepo: "https://github.com/qd9069/unblock-unwind",
+      pDeployed: "https://christinaa126.github.io/unblock-unwind/",
+    },
+    {
+      pImg: noteTaker,
+      pName: "Note Taker",
+      pSkills: "HTML/CSS/JavaScript/Express",
+      pRepo: "https://github.com/qd9069/note_taker_express_js_project",
+      pDeployed: "https://note-taker-express-js-123.herokuapp.com/",
+    },
+    {
+      pImg: jate,
+      pName: "J.A.T.E",
+      pSkills: "HTML/CSS/JavaScript/Express",
+      pRepo: "https://github.com/qd9069/jate_text_editor_project",
+      pDeployed: "https://fathomless-chamber-74811.herokuapp.com/",
+    },
+  ];
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -24,60 +70,17 @@ export default function Portfolio() {
           {/* for projects */}
           <Container maxWidth="md" sx={{ mb: 5 }}>
             <Grid container spacing={3}>
-              <Grid item xs={12} sm={6}>
-                <Project
-                  img={doggo}
-                  name="Doggo Meet Up"
-                  skills="JavaScript/Handlebars/CSS"
-                  repo="https://github.com/qd9069/doggo-meet-up"
-                  deployed="https://hidden-reaches-52880.herokuapp.com/"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Project
-                  img={weather}
-                  name="Weather Dashboard"
-                  skills="HTML/CSS/JavaScript/Bootstrap"
-                  repo="https://github.com/qd9069/weather_dashboard_server_apis_project"
-                  deployed="https://qd9069.github.io/weather_dashboard_server_apis_project/"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Project
-                  img={codeQuiz}
-                  name="Code Quiz"
-                  skills="HTML/CSS/JavaScript"
-                  repo="https://github.com/qd9069/code_quiz_api_project"
-                  deployed="https://qd9069.github.io/code_quiz_api_project/"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Project
-                  img={unblockUnwind}
-                  name="Unblock-Unwind"
-                  skills="HTML/CSS/JavaScript/jQuery"
-                  repo="https://github.com/qd9069/unblock-unwind"
-                  deployed="https://christinaa126.github.io/unblock-unwind/"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Project
-                  img={noteTaker}
-                  name="Note Taker"
-                  skills="HTML/CSS/JavaScript/Express"
-                  repo="https://github.com/qd9069/note_taker_express_js_project"
-                  deployed="https://note-taker-express-js-123.herokuapp.com/"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Project
-                  img={jate}
-                  name="J.A.T.E"
-                  skills="HTML/CSS/JavaScript/Express"
-                  repo="https://github.com/qd9069/jate_text_editor_project"
-                  deployed="https://fathomless-chamber-74811.herokuapp.com/"
-                />
-              </Grid>
+              {projects.map(({ pImg, pName, pSkills, pRepo, pDeployed }) => (
+                <Grid item xs={12} sm={6}>
+                  <Project
+                    img={pImg}
+                    name={pName}
+                    skills={pSkills}
+                    repo={pRepo}
+                    deployed={pDeployed}
+                  />
+                </Grid>
+              ))}
             </Grid>
           </Container>
         </Box>
